e89bdb6a753129af3a2d73fb3ac6b13636dd4ec5
Jason Vang <jvang@Jasons-MacBook-Air.local>
2023-10-31 21:41:35 -0500

    Ruby for and while loop iterations

---
+++ script.rb
@@ -0,0 +1,19 @@
+#1
+counter = 1
+    while counter <= 20
+        if counter % 2 == 0
+            puts "#{counter} is Even"
+            elsif 
+            puts "#{counter} is Odd"
+            end
+        counter = counter + 1
+    end 
+
+#2
+for counter in 1..20
+    if counter % 2 == 0
+        puts "#{counter} is Even"
+        elsif 
+        puts "#{counter} is Odd"
+        end
+end
